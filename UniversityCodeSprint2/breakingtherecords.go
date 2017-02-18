21f3926eb49164739fc6bee469639451cd43a76d
irlndts <kleto4kin@gmail.com>
2017-02-18 20:43:09 +0300

    breakingbest

---
+++ UniversityCodeSprint2/breakingtherecords.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(BreakingTheRecords())
+}
+
+func BreakingTheRecords() (int, int) {
+	// init input data
+	var N int
+	fmt.Scan(&N)
+
+	ar := make([]int, N)
+	for i := 0; i < N; i++ {
+		fmt.Scan(&ar[i])
+	}
+
+	//logic
+	best, worst := 0, 0
+	cbest, cworst := ar[0], ar[0]
+	for i := 1; i < N; i++ {
+		switch {
+		case ar[i] > cbest:
+			best++
+			cbest = ar[i]
+		case ar[i] < cworst:
+			worst++
+			cworst = ar[i]
+		}
+	}
+
+	return best, worst
+}
