6d9e67633de259cb4c5224eacf3d5f342df5c3b9
irlndts <kleto4kin@gmail.com>
2017-04-10 18:31:47 +0300

    Roads And Libraries

---
+++ algorithms/graphtheory/roads-and-libraries.go
@@ -0,0 +1,72 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	edges []int
+
+	counter int
+	cost    int
+	checked bool
+}
+
+func main() {
+	var Q int
+	fmt.Scan(&Q)
+	for i := 0; i < Q; i++ {
+		fmt.Println(RoadsAndLibraries())
+	}
+}
+
+func DFS(nodes *[]Node, j, i, c int) {
+	if !(*nodes)[i].checked || (*nodes)[i].counter > c {
+
+		if !(*nodes)[i].checked {
+			(*nodes)[j].cost++
+		}
+
+		(*nodes)[i].checked = true
+		(*nodes)[i].counter = c
+
+		for _, v := range (*nodes)[i].edges {
+			DFS(nodes, j, v, (*nodes)[i].counter+1)
+		}
+	}
+}
+
+func RoadsAndLibraries() int {
+	var N, M, clib, croad int
+	fmt.Scanf("%d %d %d %d", &N, &M, &clib, &croad)
+
+	nodes := make([]Node, N)
+	for i := 0; i < M; i++ {
+		var a, b int
+		fmt.Scanf("%v %v", &a, &b)
+
+		// fix the indexes
+		a--
+		b--
+
+		nodes[a].edges = append(nodes[a].edges, b)
+		nodes[b].edges = append(nodes[b].edges, a)
+	}
+
+	if clib <= croad {
+		return clib * N
+	}
+
+	for i := 0; i < N; i++ {
+		if !nodes[i].checked {
+			DFS(&nodes, i, i, 0)
+		}
+	}
+
+	result := 0
+	for i := 0; i < N; i++ {
+		if nodes[i].cost != 0 {
+			result += clib + (nodes[i].cost-1)*croad
+		}
+	}
+
+	return result
+}
